5e42e236a3bec9c328f66800df302c605b8d8726
Anton Kozyriev <sonara4mus@gmail.com>
2023-06-29 19:22:52 +0300

    feat: structure pipeline processing dir

--- pipeline/__init__.py
+++
@@ -1,3 +0,0 @@
-from .coursera import process_coursera_df
-from .stepik import process_stepik_df
-
---
+++ pipeline/run.py
@@ -0,0 +1,3 @@
+from .entity.coursera import process_coursera_df
+from .entity.stepik import process_stepik_df
+
